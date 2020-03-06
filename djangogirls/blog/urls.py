0ae62c7315106d847f3825071ea76af8acfa22cf
tungyr <rotkivem@gmail.com>
2020-03-06 10:02:55 +0300

    Added view and template for detailed blog post as well as CSS for the site; added portfolio html

--- djangogirls/blog/urls.py
+++ djangogirls/blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('portfolio/', views.portfolio, name='portfolio')
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
